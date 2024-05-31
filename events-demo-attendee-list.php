655469bb75c8bf2a4cff93abf27594950dacb3d5
Sami Dokus <sami2464@gmail.com>
2024-05-31 10:53:36 -0600

    Initial commit

---
+++ events-demo-attendee-list.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Demo Attendee List
+ * Description: Displays a list of attendees based on the current user.
+ * Version: 1.0
+ * Author: Sam Dokus
+ * Author URI: https://www.linkedin.com/in/sam-dokus/
+ * Text Domain: sdokus-demo-attendee-list
+ */
+
---
+++ src/Demo-Attendee-List/Demo-Attendee-List.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin class service provider to bootstrap the plugin.
+ *
+ * @since 1.0.0
+ */
+
+namespace Sdokus\Demo_Attendee_List;
